ab34f4aec1118d671761f0ab32fc9948b893d8dd
JingYuan <jingyuan0926@gmail.com>
2025-04-24 12:42:52 +0800

    Import header component into _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import {HeroUIProvider} from "@heroui/react";
+import Header from "@/components/Header";
 
 
 export default function App({ Component, pageProps }) {
   return (
     <HeroUIProvider>
-      < Component {...pageProps} />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </HeroUIProvider>
   )
 }
