a4ee0ea292509533d0a1500fe6b9d6df447f6ae7
JingYuan <jingyuan0926@gmail.com>
2025-04-25 23:18:07 +0800

    Remove useless codes

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import {Button} from "@heroui/react";
 
 export default function App() {
   return (
-    <div className="flex flex-wrap gap-4 items-center">
-      <Button color="default">Default</Button>
-      <Button color="primary">Primary</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="success">Success</Button>
-      <Button color="warning">Warning</Button>
-      <Button color="danger">Danger</Button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
