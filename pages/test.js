3f1c49a4e0fa2b5452aa3c383c9b4fc8d7d93287
derek2403 <derekliew0@gmail.com>
2025-04-27 22:55:51 +0800

    forms created inside test script

--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div>
       <h1>Water Quality Analysis</h1>
-      {/* Form and logic to be added */}
+      <form onSubmit={handleSubmit}>
+        <label>pH:</label>
+        <input name="pH" onChange={handleChange} />
+        <button type="submit">Analyze</button>
+      </form>
     </div>
   );
 }
