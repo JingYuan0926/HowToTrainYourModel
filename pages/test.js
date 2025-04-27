5bc4052eb1e86008c30957b28fb581602c8aec36
derek2403 <derekliew0@gmail.com>
2025-04-27 22:55:18 +0800

    this is the test script creation

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Water Quality Analysis</h1>
+      {/* Form and logic to be added */}
+    </div>
+  );
+}
