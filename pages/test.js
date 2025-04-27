8e897a5f0b24322a8ef8ee36c318dc19fd1cfcd7
derek2403 <derekliew0@gmail.com>
2025-04-27 22:56:14 +0800

    Introduce sliders and full parameter list

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
+const paramRanges = { pH: { min: 0, max: 14, step: 0.1 }, /* other params... */ };
+
 export default function Home() {
-  const [formData, setFormData] = useState({});
+  const initial = Object.keys(paramRanges).reduce((acc, key) => ({
+    ...acc,
+    [key]: (paramRanges[key].min + paramRanges[key].max) / 2,
+  }), {});
+  const [formData, setFormData] = useState(initial);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -16,8 +22,20 @@ export default function Home() {
     <div>
       <h1>Water Quality Analysis</h1>
       <form onSubmit={handleSubmit}>
-        <label>pH:</label>
-        <input name="pH" onChange={handleChange} />
+        {Object.entries(paramRanges).map(([param, range]) => (
+          <div key={param}>
+            <label>{param}</label>
+            <input
+              type="range"
+              name={param}
+              min={range.min}
+              max={range.max}
+              step={range.step}
+              value={formData[param]}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
         <button type="submit">Analyze</button>
       </form>
     </div>
