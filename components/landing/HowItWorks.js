d2a0b542f1e1be117619eacdd76633251b1afdb9
Liew Qi Jian <146611128+derek2403@users.noreply.github.com>
2025-05-18 22:25:30 +0800

    Ensure it is perfect square in how it works

--- components/landing/HowItWorks.js
+++ components/landing/HowItWorks.js
@@ -15,7 +15,7 @@ export default function HowItWorks() {
         {/* Grid section with continuous lines */}
         <div className="relative">
           {/* Grid container with increased height to match width (square) */}
-          <div className="relative h-[1024px] max-w-5xl mx-auto">
+          <div className="relative w-full max-w-5xl aspect-square mx-auto">
             {/* Horizontal lines that extend beyond the container */}
             <div className="absolute top-0 w-[200vw] left-[-50vw] h-px bg-gray-300"></div>
             <div className="absolute top-1/2 w-[200vw] left-[-50vw] h-px bg-gray-300"></div>
