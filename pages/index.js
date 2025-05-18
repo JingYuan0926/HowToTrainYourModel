2094c5fa0e077401bce243da02305ba95b4b2521
JingYuan <jingyuan0926@gmail.com>
2025-05-18 19:58:20 +0800

    Remove useless import for clean code

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import Hero from '../components/landing/Hero';
-import Problem from '../components/landing/Problem';
-import Solution from '../components/landing/Solution';
-import KeyFeatures from '../components/landing/KeyFeatures';
-import TEE from '../components/landing/TEE';
-import Roadmap from '../components/landing/Roadmap';
-import CallToAction from '../components/landing/CallToAction';
+
 import Footer from '../components/landing/Footer';
 import HowItWorks from '../components/landing/HowItWorks';
 import Features from '../components/landing/Features';
